d3a25b4a4b78e4d25190e7b2a461714f59dc5650
SH0WR0V <sm.shahriar1230@gmail.com>
2023-04-29 02:01:09 +0600

    conditional control

--- src/components/List.js
+++ src/components/List.js
@@ -8,15 +8,29 @@ export const List = () => {
         {id: 1002, name: "Play Pubg", completed: false},
         {id: 1003, name: "Take Dinner", completed: false}
     ])
+
+    const [show, setShow] = useState(true);
+    const [view, setView] = useState("View");
+
+    function deleteTask(id){
+        setLists(lists.filter((list) => list.id !== id ))
+    }
+
+    function change() {
+        setShow(!show);
+        setView( view === "View" ? "Hide" : "View");
+    }
+
   return (
 
         <div className='App'>
             <h2>Task List</h2>
             <ul>
-                {lists.map((list) => (
-                    <li>
+                <button className='trigger' onClick={change}>{view}</button>
+                {show && lists.map((list) => (
+                    <li key={list.id} className = {list.completed ? 'completed' : 'incomplete'}>
                         <span>{list.id} - {list.name}</span>
-                        <button className='delete'>Delete</button>
+                        <button onClick = {() => deleteTask(list.id)} className='delete'>Delete</button>
                     </li>
                 ))}
             </ul>
