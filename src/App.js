a415ddcdb2f5c06c0f9ce6055dd46b97bcb52616
SH0WR0V <sm.shahriar1230@gmail.com>
2023-04-28 21:03:55 +0600

    count

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from 'react';
+import { useState } from 'react';
+import {Header} from './components/Header';
+import './App.css';
 
 export const App = () => {
+
+  const [count, setCount] = useState(0);
+
+  function handleAdd(){
+    setCount(count + 1);
+  }
+
+  function handleSub(){
+    setCount(count - 1);
+  }
+
+  function handleReset(){
+    setCount(0);
+  }
+
   return (
-    <div>
+    <div className="App">
+        <Header/>
         <h1>Welcome Shahriar!</h1>
+
+        <div className="box">
+          <p>{count}</p>
+          <button onClick={handleAdd} className="add">Add</button>
+          <button onClick={handleSub} className="sub">Sub</button>
+          <button onClick={handleReset} className="reset">Reset</button>
+        </div>
     </div>
   )
 }
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import pp from '../assets/profile_pic.jpg';
+
+export const Header = () => {
+  return (
+    <div>
+        <img className="img" src={pp} alt="" />
+    </div>
+  )
+}
